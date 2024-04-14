23a20d314399c6df88a24ab17728584933ff728b
Luftjunkie <krawczyk0606@gmail.com>
2024-04-14 16:30:47 +0200

    push another

--- assets/meshes/LanguageMesh.tsx
+++ assets/meshes/LanguageMesh.tsx
@@ -6,6 +6,8 @@ import {
   Decal,
   Float,
   OrbitControls,
+  Scroll,
+  ScrollControls,
   useTexture,
 } from '@react-three/drei';
 import {
@@ -26,20 +28,20 @@ function LanguageMesh({texturePath, position, techName, backgroundColour}: Props
 
 
   return (
-    <Float floatIntensity={1.25} speed={1.75} rotationIntensity={0.6}>
+    <Scroll>
+         <Float floatIntensity={1.25} speed={1.75} rotationIntensity={0.6}>
 
-      <mesh castShadow receiveShadow scale={2.25}>
-      
-        <sphereGeometry  />
-        <meshStandardMaterial polygonOffset polygonOffsetFactor={-5} flatShading color={backgroundColour ? backgroundColour : 'lightblue'}/>
-      <Decal rotation={[2 * Math.PI, 0, 6.25]} position={[0, 0, 1]} map={texture} scale={0.9} />
-      </mesh>
-      
-      
+<mesh castShadow receiveShadow scale={2.25}>
 
-    </Float>
+  <sphereGeometry  />
+  <meshStandardMaterial polygonOffset polygonOffsetFactor={-5} flatShading color={backgroundColour ? backgroundColour : 'lightblue'}/>
+<Decal rotation={[2 * Math.PI, 0, 6.25]} position={[0, 0, 1]} map={texture} scale={0.9} />
+</mesh>
 
 
+
+</Float>
+    </Scroll>
   );
 }
 
@@ -49,7 +51,11 @@ function MeshCanvas({texturePath, position, techName, backgroundColour}:Props) {
     <ambientLight />
     <directionalLight />
     
+    <ScrollControls>
+
     <LanguageMesh backgroundColour={backgroundColour} position={position} texturePath={texturePath} techName={techName} />
+    </ScrollControls>
+
 
     {/* <Text3D position={[-1, -2, 2]} scale={0.25} font={'./fonts/RobotoRegular.json'}>
       {techName}
